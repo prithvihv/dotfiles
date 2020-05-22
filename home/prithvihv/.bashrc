3c7a94b2cff60c12ab91ba4bce193e56a3c4fd0d
prithvihv <hvprithvi@gmail.com>
2020-05-22 19:52:31 +0530

    windows set up

--- home/prithvihv/.bashrc
+++
@@ -1,3 +0,0 @@
-if command -v fzf-share >/dev/null; then
-  source "$(fzf-share)/key-bindings.bash"
-fi
---
+++ windows.setup.cmd
@@ -0,0 +1,10 @@
+// intalll choco first 
+
+choco install pass-winmenu
+choco install vscode
+choco install netcat
+choco install git
+choco install nodejs-lts
+
+git clone git@github.com:prithvihv/allPasswords.git 
+git clone git@github.com:prithvihv/dotfiles.git
